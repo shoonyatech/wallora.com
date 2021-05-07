ad2c2cb51e039e6f24bc4a5895d1a73c96b40299
aryamantodkar <aryamant20@gmail.com>
2021-05-08 01:50:52 +0530

    feat(Homepage_Layout):Created homepage layout

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,24 @@
 import Head from "next/head";
+import styles from '../styles/index.module.css';
 
 export default function Home() {
   return (
-    <div className="container">
+    <div className={styles.container}>
       <Head>
         <title>Wallora</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
-      <main>
+      <header className={styles.header}>
+        This is header.
+      </header>
+      <main className={styles.main}>
         <h1 className="title">
           Welcome to <a href="https://wallora.com">Wallora.com!</a>
         </h1>
       </main>
+      <footer className={styles.footer}>
+        This is footer
+      </footer>
     </div>
   );
 }
