927b490c99bbb32ca9f0d3ebcffde71e4645886a
Priyanka Jayaswal <priyankajayaswal025@gmail.com>
2021-05-09 01:07:46 +0530

    feat(auth): integrate Auth0 signup/in using email/password

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,13 @@
 import 'tailwindcss/tailwind.css';
 import {AppProps} from 'next/app'
+import { UserProvider } from '@auth0/nextjs-auth0';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <UserProvider>
+      <Component {...pageProps} />
+    </UserProvider>
+  );
 }
 
 export default MyApp
