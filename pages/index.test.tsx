19abe1c6876f692ce6c0dae66b3b0bb9937e7f42
Souvik Basu <souvikbasu@gmail.com>
2021-05-15 00:43:48 -0700

    fix(build): remove jest test for pages as prod build fails for jest/globals like describe

--- pages/index.test.tsx
+++
@@ -1,9 +0,0 @@
-import { render,cleanup } from '@testing-library/react';
-import Home from './index'
-
-
-describe('Home component', () => {
- test('it renders', () => {
-   render(<Home />);
- });
-})
