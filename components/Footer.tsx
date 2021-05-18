f8ce5679fe7ffc0f6ac5912c97c8388f9cc89037
aryamantodkar <aryamant20@gmail.com>
2021-05-18 11:14:26 +0530

    feat(Footer): create Terms of Use and Privacy Policy pages

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,14 +1,18 @@
+
 export default function Footer(){
     return(
-        <div className="bg-blue-200 p-5 mt-2 flex justify-between">
-                <span className='mt-auto mb-auto'>This is footer</span>
+        <div className="bg-blue-200 pl-5 pr-5 pt-2 pb-2 mt-2 flex justify-between">
+                <span className='mt-auto mb-auto'>
+                    <a href='/terms' className='pr-12' target='_blank'>Terms</a>
+                    <a href='/privacy' target='_blank'>Privacy Policy</a>
+                </span>
                 <span className='flex flex-row'>
                     <div className='bg-sprite-background bg-no-repeat' style={{width:60,height:60}}></div>
-                    <div className='bg-sprite-background bg-no-repeat' style={{width:56,height:55, backgroundPositionX:-60}}></div>
-                    <div className='bg-sprite-background bg-no-repeat' style={{width:56,height:55, backgroundPositionX:-118}}></div>
-                    <div className='bg-sprite-background bg-no-repeat' style={{width:56,height:55, backgroundPositionX:-176}}></div>
-                    <div className='bg-sprite-background bg-no-repeat' style={{width:56,height:55, backgroundPositionX:-234}}></div>
-                    <div className='bg-sprite-background bg-no-repeat' style={{width:56,height:55, backgroundPositionX:-292}}></div>
+                    <a href='https://www.facebook.com/wallora/' target='_blank'><div className='bg-sprite-background bg-no-repeat' style={{width:56,height:55, backgroundPositionX:-60}}></div></a>
+                    <a href='https://twitter.com/WalloraInc' target='_blank'><div className='bg-sprite-background bg-no-repeat' style={{width:56,height:55, backgroundPositionX:-118}}></div></a>
+                    <a href='https://plus.google.com/115182973503232931446' target='_blank'><div className='bg-sprite-background bg-no-repeat' style={{width:56,height:55, backgroundPositionX:-176}}></div></a>
+                    <a href='https://www.linkedin.com/company/wallora/about/' target='_blank'><div className='bg-sprite-background bg-no-repeat' style={{width:56,height:55, backgroundPositionX:-234}}></div></a>
+                    <a href='https://in.pinterest.com/WalloraInc/' target='_blank'><div className='bg-sprite-background bg-no-repeat' style={{width:56,height:55, backgroundPositionX:-292}}></div></a>
                 </span>
         </div>
     )
