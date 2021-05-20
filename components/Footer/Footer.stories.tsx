0d595aeecf439244bcc1594aceb20e05671061d8
aryamantodkar <aryamant20@gmail.com>
2021-05-20 14:03:29 +0530

    feat(footer): add terms and privacy links

---
+++ components/Footer/Footer.stories.tsx
@@ -0,0 +1,10 @@
+import Footer from '.';
+
+export default {
+    title: 'Footer',
+    component: Footer,
+}
+
+const Template = args => <Footer/>
+
+export const Default = Template.bind({});
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Head from 'next/head';
 import Header from '../components/Header';
-import Footer from '../components/Footer';
+import Footer from '../components/Footer/index';
 import { useUser } from '@auth0/nextjs-auth0';
 
 export default function Home() {
