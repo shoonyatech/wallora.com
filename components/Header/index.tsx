b1888ba8b76e205f370f09cf0a6b6b4a1ca6b3e7
Priyanka Jayaswal <priyankajayaswal025@gmail.com>
2021-05-22 23:12:49 +0530

    adding material-ui header and footer

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -1,20 +1,22 @@
-import Button from '@material-ui/core/Button'
+import { AppBar, Button } from '@material-ui/core'
 import React from 'react'
 
 const Header = ({ user }: any) => (
-  <div className="bg-secondary pl-10  p-5 flex justify-between">
-    <img src="./images/wallora-logo-170x53.jpeg" alt="This is Wallora logo." />
-    <span className="text-primary">This is header</span>
-    {user ? (
-      <Button variant="contained" color="primary" href="/api/auth/logout">
-        LOGOUT
-      </Button>
-    ) : (
-      <Button variant="contained" color="primary" href="/api/auth/login">
-        LOGIN
-      </Button>
-    )}
-  </div>
+  <AppBar position="static">
+    <div className="bg-secondary pl-10  p-5 flex justify-between">
+      <img src="./images/wallora-logo-170x53.jpeg" alt="This is Wallora logo." />
+      <span className="text-primary">This is header</span>
+      {user ? (
+        <Button variant="contained" color="primary" href="/api/auth/logout">
+          LOGOUT
+        </Button>
+      ) : (
+        <Button variant="contained" color="primary" href="/api/auth/login">
+          LOGIN
+        </Button>
+      )}
+    </div>
+  </AppBar>
 )
 
 export default Header
