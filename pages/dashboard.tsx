0cee437cf24b9ad983a3710f3ad4c1c314fc5897
hussamkhatib <hussamkhatib20@gmail.com>
2021-06-22 16:08:51 +0530

    feat(_app.tsx) : add route guard to protected url's

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -1,26 +1,17 @@
-import { useUser } from '@auth0/nextjs-auth0'
 import React from 'react'
 
 import BaseLayout from '../components/common/BaseLayout'
-import Loader from '../components/common/Loader'
 import PlannedExpensesChart from '../components/dashboard/PlannedExpensesChart'
 import UserSettings from '../components/dashboard/UserSettings'
 import ClientSideRendering from '../lib/client-side-rendering'
 
 function DashBoard() {
-  const { user, error, isLoading } = useUser()
-
-  if (isLoading || error)
-    return (
-      <div>
-        <Loader open={isLoading} error={error} />
-      </div>
-    )
-
   return (
     <ClientSideRendering>
       <UserSettings />
-      <div className="ml-8">{user ? <PlannedExpensesChart /> : null}</div>
+      <div className="ml-8">
+        <PlannedExpensesChart />
+      </div>
     </ClientSideRendering>
   )
 }
