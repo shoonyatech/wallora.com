39b435426cc339c94c29fbc5aeb03e89fd2c9084
hussamkhatib <hussamkhatib20@gmail.com>
2021-06-26 12:52:55 +0530

    fix(home): remove home page content when user is logged in

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,24 @@
+import { useUser } from '@auth0/nextjs-auth0'
+import { useRouter } from 'next/router'
 import React from 'react'
 
 import BaseLayout from '../components/common/BaseLayout'
+import Loader from '../components/common/Loader'
 import MainContent from '../components/home/MainContent'
 
 export default function Home() {
+  const router = useRouter()
+  const { user, error, isLoading } = useUser()
+
+  if (isLoading || error)
+    return (
+      <div>
+        <Loader open={isLoading} error={error} />
+      </div>
+    )
+  if (user) {
+    router.push('/dashboard')
+  }
   return (
     <div>
       <MainContent />
