d74900e66294d1a4a708ee37752fe230018beef8
hussamkhatib <hussamkhatib20@gmail.com>
2021-08-07 16:36:24 +0530

    fix(auth) : change routeguard for protected routes from using next.js router to auth0

--- pages/finance/plan.tsx
+++ pages/finance/plan.tsx
@@ -1,13 +1,10 @@
+import { withPageAuthRequired } from '@auth0/nextjs-auth0'
 import React from 'react'
 
 import FinanceSwitch from '../../components/finance/FinanceSwitch'
 
-function Plan() {
-  return (
-    <div className="grid grid-cols-financeHeader h-16">
-      <FinanceSwitch actualOrPlan="Plan" />
-    </div>
-  )
-}
-
-export default Plan
+export default withPageAuthRequired(() => (
+  <div className="grid grid-cols-financeHeader h-16">
+    <FinanceSwitch actualOrPlan="Plan" />
+  </div>
+))
