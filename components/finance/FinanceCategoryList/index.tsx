38a0f80342a5f558b3fa1cd1b5e63dc913b3616f
kit486759 <kit486759@gmail.com>
2021-09-17 16:20:53 -0700

    feat(plan.tsx): feature add percentage of each expense in plan page

--- components/finance/FinanceCategoryList/index.tsx
+++ components/finance/FinanceCategoryList/index.tsx
@@ -1,23 +1,43 @@
 import React from 'react'
 
-function FinanceCategoryList({ item, rowIndex, activeRow, summaryOnClick }: any) {
+function FinanceCategoryList({ item, rowIndex, activeRow, summaryOnClick, actualsOrPlan, percentage }: any) {
   return (
     <>
-      <div
-        onClick={() => summaryOnClick(item)}
-        // onKeyDown={() => summaryOnClick(item)}
-        aria-hidden="true"
-        role="button"
-        tabIndex={0}
-        className={`p-2.5 flex-1 cursor-pointer ${rowIndex === activeRow ? 'bg-workItemActive' : 'bg-workItem'}`}
-      >
-        {item.name}
-      </div>
-      <div
-        className={`p-2.5 w-24 text-center ${
-          item.incomeOrExpense === 'expense' ? 'bg-plannedExpense' : 'bg-plannedIncome'
-        }`}
-      />
+      {actualsOrPlan === 'actuals' ? (
+        <>
+          <div
+            onClick={() => summaryOnClick(item)}
+            aria-hidden="true"
+            role="button"
+            tabIndex={0}
+            className={`p-2.5 flex-1 cursor-pointer ${rowIndex === activeRow ? 'bg-workItemActive' : 'bg-workItem'}`}
+          >
+            {item.name}
+          </div>
+          <div
+            className={`p-2.5 w-24 text-center ${
+              item.incomeOrExpense === 'expense' ? 'bg-plannedExpense' : 'bg-plannedIncome'
+            }`}
+          />
+        </>
+      ) : (
+        <>
+          <div
+            onClick={() => summaryOnClick(item)}
+            aria-hidden="true"
+            role="button"
+            tabIndex={0}
+            className={`p-2.5 flex-1 cursor-pointer ${rowIndex === activeRow ? 'bg-workItemActive' : 'bg-workItem'}`}
+          >
+            {item.name}
+          </div>
+          <div className={` w-24`}>
+            <div style={{ width: `${percentage}%` }} className="p-2.5 text-center bg-red-200">
+              {percentage}%
+            </div>
+          </div>
+        </>
+      )}
     </>
   )
 }
